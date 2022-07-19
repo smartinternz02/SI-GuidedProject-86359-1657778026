3dbb417ca61b61b8b0bc18725223dcc31b14bdfe
ramyaravibeena <109520511+ramyaravibeena@users.noreply.github.com>
2022-07-19 09:04:39 +0530

    Add files via upload

---
+++ Flask/app_fruit.py
@@ -0,0 +1,31 @@
+import numpy as np
+import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+model=load_model("fruit.h5")
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/predict',methods=['GET','POST'])
+def upload():
+    if request.method=='POST':
+        f=request.files['image']
+        basepath=os.path.dirname(__file__)
+        filepath=os.path.join(basepath,'uploads',f.filename)
+        f.save(filepath)
+        img=image.load_img(filepath,target_size=(128,128))
+        x=image.img_to_array(img)
+        x=np.expand_dims(x,axis=0)
+        pred=np.argmax(model.predict(x),axis=1)
+        index=['Apple___Black_rot','Apple___healthy','Corn_(maize)___Northern_Leaf_Blight','Corn_(maize)___healthy','Peach___Bacterial_spot','Peach___healthy']
+        text="The Classified Fruit is : " +str(index[pred[0]])
+    return text
+
+if __name__=='__main__':
+    app.run(debug=False)
